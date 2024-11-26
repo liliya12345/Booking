da05a7ed425d236f64bb4d8ef95a85dbe093a074
liliya <liliya.sayfutdinova1@gmail.com>
2024-11-26 22:04:31 +0100

    Add addplace fun

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        addPlace("Paris");
+    }
+   static void addPlace(String place){
+        System.out.println(place);
     }
 }
