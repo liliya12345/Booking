e6a3b55fa3a8e816af60b4f43dd9d1b4674adc1f
liliya <liliya.sayfutdinova1@gmail.com>
2024-11-26 22:16:38 +0100

    add date fun

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        adddate(LocalDate.now());
+    }
+    static void adddate(LocalDate date){
+        System.out.println(date);
     }
 }
